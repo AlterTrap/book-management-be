70d1d38f9806c33c8544c5c2490cfc81374fc562
KhaiNd3 <kekocoten19@gmail.com>
2023-09-06 00:21:35 +0900

    update route with checking param

--- src/routes/book.js
+++ src/routes/book.js
@@ -4,9 +4,13 @@ const bookController = require('../controllers/bookController');
 
 router.get('/', (req, res) => bookController.find(req, res));
 
+router.get('/*', (req, res) => res.json('error'));
+
 router.post('/', (req, res) => bookController.create(req, res));
 
 router.patch('/:id', (req, res) => bookController.update(req, res));
+
+router.delete('/', (req, res) => res.json('error'));
 
 router.delete('/:id', (req, res) => bookController.destroy(req, res));
 
