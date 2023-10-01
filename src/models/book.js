12860b2e87ed0e3855c020ceeabe27bcc0809775
KhaiNd3 <kekocoten19@gmail.com>
2023-10-01 19:05:44 +0900

    fix bug

--- src/models/book.js
+++ src/models/book.js
@@ -1,7 +1,6 @@
 const dataTypes = require('sequelize/lib/data-types');
 const { connection } = require('../utils/dbConnection');
 const { DataTypes, Sequelize } = require('sequelize');
-const User = require('../models/user');
 
 const Book = connection.define(
   'Book',
@@ -15,8 +14,5 @@ const Book = connection.define(
     timestamps: true,
   }
 );
-
-// User.hasMany(Book, { as: 'books', foreignKey: 'userId' });
-// Book.belongsTo(User, { foreignKey: 'userId' });
 
 module.exports = { Book };
