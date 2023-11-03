f4f9bf1c34d0401fb6fba58f1a8b81ae27a7c963
KhaiNd3 <kekocoten19@gmail.com>
2023-11-04 01:33:21 +0900

    add authentication function

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const session = require('express-session');
-const passport = require('./src/utils/passportconfig');
+const passport = require('./src/utils/passportJwtConfig');
 
 const app = express();
 const exampleRouter = require('./src/routes/example');
@@ -11,19 +10,9 @@ const cors = require('cors');
 
 const port = 3000;
 
-app.use(
-  session({
-    secret: process.env.DEV_DB_SECRET,
-    resave: true,
-    saveUninitialized: true,
-    cookie: { maxAge: 3600000 },
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.json());
 app.use(cors());
+app.use(passport.initialize());
 
 app.use('/api/books', bookRouter);
 app.use('/api/examples', exampleRouter);
--- src/routes/book.js
+++ src/routes/book.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
+const passport = require('passport');
 
-router.get('/', (req, res) => bookController.find(req, res));
-router.post('/', (req, res) => bookController.create(req, res));
-router.patch('/:id', (req, res) => bookController.update(req, res));
-router.delete('/:id', (req, res) => bookController.destroy(req, res));
+const requireAuth = passport.authenticate('jwt', { session: false });
+
+router.get('/', requireAuth, (req, res) => bookController.find(req, res));
+router.post('/', requireAuth, (req, res) => bookController.create(req, res));
+router.patch('/:id', requireAuth, (req, res) =>
+  bookController.update(req, res)
+);
+router.delete('/:id', requireAuth, (req, res) =>
+  bookController.destroy(req, res)
+);
 
 module.exports = router;
